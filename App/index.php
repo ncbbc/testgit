b719643c001a990f8c4cd9f84f5d6c0683806403
赵大阳 <zhaoyang02@vchangyi.com>
2021-02-08 18:33:52 +0800

    test

---
+++ App/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2021/2/8 0008
+ * Time: 18:33
+ */
+
+class index
+{
+    public function test()
+    {
+        echo 'index';
+    }
+}
